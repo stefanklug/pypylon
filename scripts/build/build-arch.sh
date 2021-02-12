1728b31c01aaa4969dce14eb58c239f01f3494fd
Stefan Klug <stefan.klug@baslerweb.com>
2021-02-12 16:21:06 +0100

    Add pep-600 based manylinux builds

--- scripts/build/build-arch.sh
+++ scripts/build/build-arch.sh
@@ -89,6 +89,10 @@ case $PLATFORM_TAG in
     linux_i686)             QEMU_ARCH="i386";     BASE_IMAGE="i386/$BASE_IMAGE";                     PYLON_ARCH=x86 ;        CMD_WRAPPER=linux32 ;;
     linux_armv7l)           QEMU_ARCH="arm";      BASE_IMAGE="arm32v7/$BASE_IMAGE";                  PYLON_ARCH=armhf        CMD_WRAPPER=linux32 ;;
     linux_aarch64)          QEMU_ARCH="aarch64";  BASE_IMAGE="arm64v8/$BASE_IMAGE";                  PYLON_ARCH=aarch64 ;;
+    manylinux_2_17_x86_64)  QEMU_ARCH="x86_64";   BASE_IMAGE="amd64/$BASE_IMAGE";                    PYLON_ARCH=x86_64 ;;
+    manylinux_2_17_i686)    QEMU_ARCH="i386";     BASE_IMAGE="i386/$BASE_IMAGE";                     PYLON_ARCH=x86 ;        CMD_WRAPPER=linux32 ;;
+    manylinux_2_17_armv7l)  QEMU_ARCH="arm";      BASE_IMAGE="arm32v7/$BASE_IMAGE";                  PYLON_ARCH=armhf        CMD_WRAPPER=linux32 ;;
+    manylinux_2_17_aarch64) QEMU_ARCH="aarch64";  BASE_IMAGE="arm64v8/$BASE_IMAGE";                  PYLON_ARCH=aarch64 ;;
     manylinux2014_x86_64)   QEMU_ARCH="x86_64";   BASE_IMAGE="quay.io/pypa/manylinux2014_x86_64";    PYLON_ARCH=x86_64 ;;
     manylinux2014_i686)     QEMU_ARCH="i386";     BASE_IMAGE="quay.io/pypa/manylinux2014_i686";      PYLON_ARCH=x86 ;        CMD_WRAPPER=linux32 ;;
     manylinux2014_aarch64)  QEMU_ARCH="aarch64";  BASE_IMAGE="quay.io/pypa/manylinux2014_aarch64";   PYLON_ARCH=aarch64 ;;
